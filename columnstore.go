47ec3d1eed74be8eb5d506ef435ce1813dbb4b06
Mohamad Karam Kassem <karam.kass@gmail.com>
2016-05-03 10:42:00 +0200

    columstore functions done!

--- columnstore.go
+++ columnstore.go
@@ -3,36 +3,31 @@ package csgo
 // CreateRelation creates a new relation within the column store and returns
 // an object reference.
 func (c ColumnStore) CreateRelation(tabName string, sig []AttrInfo) Relationer {
-	/*
-		Relation: Relation = {
-			- Name: string
-			- Columns: Column[] = [
-					- Signature: AttrInfo = {
-							- Name: string
-							- Type: DataTypes
-							- Enc: Compression
-						}
-					- Data: slice
-				]
-		}
-	*/
-	// Relations is the mapping of relation names to their object reference.
-	// Relations map[string]Relationer
 	if (c.Relations == nil) {
 		c.Relations = make(map[string]Relationer)
 	}
 
 	r, exist := c.Relations[tabName]
+
 	if (!exist) {
-		var rel Relation
-		rel.Name = tabName
-		rel.Columns = make([]Column, len(sig), len(sig))
+		var relation Relation
+		relation.Name = tabName
+		relation.Columns = make([]Column, len(sig), len(sig))
 
 		for index := 0; index < len(sig); index++ {
-			rel.Columns[index].Signature = sig[index]
+			relation.Columns[index].Signature = sig[index]
+			if (sig[index].Type == INT) {
+				relation.Columns[index].Data = make([]int, 0)
+			} else {
+				if (sig[index].Type == FLOAT) {
+					relation.Columns[index].Data = make([]float64, 0)
+				} else {
+				relation.Columns[index].Data = make([]string, 0)
+				}
+			}
 		}
-
-		return rel
+		c.Relations[tabName] = relation
+		return relation
 	}
 
 	return r // die Alte vorhandene Relation, die existiert ist
@@ -41,5 +36,14 @@ func (c ColumnStore) CreateRelation(tabName string, sig []AttrInfo) Relationer {
 // GetRelation returns the object reference of a relation associated with the
 // passed relation name.
 func (c ColumnStore) GetRelation(relName string) Relationer {
+	if (c.Relations == nil) {
+		return nil
+	} else {
+		relation, exist := c.Relations[relName]
+		if (exist) {
+			return relation
+		}
+	}
+
 	return nil
 }
--- csgo/main.go
+++ csgo/main.go
@@ -6,27 +6,53 @@ import (
 )
 
 func test_case() {
-	var cs ColumnStore
+	c := ColumnStore{Relations: make(map[string]Relationer)}
+
+	c.CreateRelation("SalesJan2009", []AttrInfo {
+		{Name: "Transaction_date", 	Type: STRING, Enc: NOCOMP},
+		{Name: "Product",	 					Type: STRING, Enc: NOCOMP},
+		{Name: "Price", 						Type: INT, 		Enc: NOCOMP},
+		{Name: "Payment_Type", 			Type: STRING, Enc: NOCOMP},
+		{Name: "Name", 							Type: STRING, Enc: NOCOMP},
+		{Name: "City", 							Type: STRING, Enc: NOCOMP},
+		{Name: "State", 						Type: STRING, Enc: NOCOMP},
+		{Name: "Country", 					Type: STRING, Enc: NOCOMP},
+		{Name: "Account_Created", 	Type: STRING, Enc: NOCOMP},
+		{Name: "Last_Login", 				Type: STRING, Enc: NOCOMP},
+		{Name: "Latitude", 					Type: FLOAT, 	Enc: NOCOMP},
+		{Name: "Longitude", 				Type: FLOAT, 	Enc: NOCOMP},
+	})
+
+	//c.GetRelation("SalesJan2009").Load("SalesJan2009.csv", ',')
+	fmt.Println("DEBUG: HIER 2")
+	r := c.GetRelation("SalesJan2009").Select(AttrInfo{Name: "Country", Type: STRING, Enc: NOCOMP}, NEQ, "United States").Scan([]AttrInfo{
+		{Name: "Name", Type: STRING, Enc: NOCOMP},
+		{Name: "Latitude", Type: FLOAT, Enc: NOCOMP},
+		{Name: "Longitude", Type: FLOAT, Enc: NOCOMP},
+	})
+	r.Print()
+
 
 	c.CreateRelation("Sudent", []AttrInfo{
 		{Name: "id", Type: INT, Enc: NOCOMP},
-		{Name: "name", Type: STRING, Enc: NOCOMP}
+		{Name: "name", Type: STRING, Enc: NOCOMP},
 	})
-	fmt.Println("Relation 'Student' has been created!")
 
 	c.CreateRelation("Result", []AttrInfo{
 		{Name: "id", Type: INT, Enc: NOCOMP},
 		{Name: "student_id", Type: INT, Enc: NOCOMP},
-		{Name: "result", Type: FLOAT, Enc: NOCOMP}
+		{Name: "result", Type: FLOAT, Enc: NOCOMP},
 	})
-	fmt.Println("Relation 'Result' has been created!")
 
-	for k, r in c.Relations {
-		r.Print()
+	for _, r := range c.Relations {
+			r.Print()
 	}
 }
 
 func main() {
+	
+	test_case()
+
 	/*
 	fmt.Println("This might become a useful console utility for our ColumnStore database")
 	fmt.Println()
